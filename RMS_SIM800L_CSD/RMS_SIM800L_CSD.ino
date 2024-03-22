537d742668b1563c3054b4dbefa785e1481d559a
Md. Nazmul Hossain Shakil <86412058+NHShakil@users.noreply.github.com>
2024-03-22 19:57:38 +0600

    Final Testing Code V 1.0
    22 MAR 2024 with optocupler

--- RMS_SIM800L_CSD/RMS_SIM800L_CSD.ino
+++ RMS_SIM800L_CSD/RMS_SIM800L_CSD.ino
@@ -1,9 +1,7 @@
 #include "defines.h"
 
 void setup() {
-  DEBUG.begin("ESP32test");
-  //DEBUG.begin(DEBUG_BAUD_RATE);
-  // UART2 Configuration
+  DEBUG.begin("WALTON_IOT_DEV_46416");
   MODEM.begin(MODEM_BAUD_RATE);
   Wire.begin();
   // UART1 Configuration
@@ -23,50 +21,35 @@ void setup() {
   //vTaskDelete(NULL);
   //Configurations
   test_sim800_module();
-  delay(1000);
-  gsm_config_gprs();
-  delay(1000);
-  //ESP.restart();
+    delay(1000);
+  for (int i=0;i<5;i++) {
+    gsm_config_gprs();
+    delay(1000);
+    //ESP.restart();
+  }
 }
 
 void loop() {
-
-  // readCompModel();
-  // delay(10);
-  // if (mode == 0) {
-  //   DEBUG.println("Func Mod : 0");
-  //   Send_GET_Rqst(URL + DEV_ID + ";" + MOB_NO + ";" + sigLvl + ";" + ";" + ";;" + ";;" + ";;", "0");
-  //   DEBUG.print("*********************");
-  //   DEBUG.println(srvResPns);
-  //   DEBUG.print("#####################");
-  //   explode(",", srvResPns);
-  //   delay(1000);
-  // }
-  // //if (mode = 1) {
-  // readCompModel();
-  // delay(10);
-  // DEBUG.println("Func Mod : 1");
-  // //Send_GET_Rqst(URL + DEV_ID + ";" + MOB_NO + ";" + sigLvl + ";" + ODU_PAC_ONE + ";" + ODU_PAC_TWO + ";" + IDU_EE_DATA + ";" + ODU_EE_DATA, "0");
-  // Send_GET_Rqst(URL + DEV_ID + ";" + MOB_NO + ";" + sigLvl + ";" + ODU_PAC_ONE + ";" + ODU_PAC_TWO + ";" + ODU_PAC_THREE + ";" + IDU_EE_DATA + ";" + ODU_EE_DATA, "0");
-  // DEBUG.print("*********************");
-  // DEBUG.println(srvResPns);
-  // DEBUG.print("#####################");
-  // explode(",", srvResPns);
-  // delay(1000);
-  // //}
   readCompModel();
   delay(100);
   eePortType = detect_ee();
   delay(100);
   Send_GET_Rqst(URL + DEV_ID + ";" + MOB_NO + ";" + sigLvl + ";" + ODU_PAC_ONE + ";" + ODU_PAC_TWO + ";" + ODU_PAC_THREE + ";" + eePortType + ";" + IDU_EE_DATA + ";" + ODU_EE_DATA, "0");
-  DEBUG.print("*********************");
-  DEBUG.println(srvResPns);
-  DEBUG.print("#####################");
   explode(",", srvResPns);
+  if (IDU_EE_RD_FLG == 1) {
+    read_EE_data(255);
+  }
+  if (ODU_EE_RD_FLG == 1) {
+    read_EE_data(1023);
+  }
+
   delay(1000);
-
-
   srvResPns = "";
+  IDU_EE_RD_FLG = 0;
+  IDU_EE_WR_FLG = 0;
+  ODU_EE_RD_FLG = 0;
+  ODU_EE_WR_FLG = 0;
+  eePortType = 0;
 }
 void explode(char delim[], String rcv_Str) {
   int str_len = rcv_Str.length() + 1;
@@ -76,7 +59,6 @@ void explode(char delim[], String rcv_Str) {
   char *ptr = strtok(char_array, delim);
   int decimalVal = 0;
   int strt_pos = 0, end_pos = 0, totl_Elmnt = 0;
-  DEBUG.println("-----------> Start Exploding: ");
   while (ptr != NULL) {
     int strLen = strlen(ptr);
     for (int i = 0; i < strLen; i++) {
@@ -85,37 +67,31 @@ void explode(char delim[], String rcv_Str) {
     if (a > 0) {
       if (a == 1) {
         mode = decimalVal;
-        DEBUG.print("\t Mode: ");
-        DEBUG.print(mode);
+        DEBUG.print("Mode : ");
+        DEBUG.println(mode);
+        if (mode == 2) { IDU_EE_WR_FLG = 1; }
+        if (mode == 3) { ODU_EE_WR_FLG = 1; }
+        if (mode == 4) { IDU_EE_RD_FLG = 1; }
+        if (mode == 5) { ODU_EE_RD_FLG = 1; }
       } else if (a == 2) {
+        if (decimalVal < 0) { break; }
         strt_pos = decimalVal;
-        DEBUG.print("\t Start Pos: ");
+        DEBUG.print("\t Start Pos : ");
         DEBUG.println(strt_pos);
-        //a = strt_pos;
       } else if (a == 3) {
+        if (decimalVal < 0) { break; }
         end_pos = decimalVal;
         DEBUG.print("\t END Pos: ");
         DEBUG.println(end_pos);
-        //a = strt_pos;
       } else if (a == 4) {
         totl_Elmnt = decimalVal + 4;
         DEBUG.print("\t TOTL ELM : ");
         DEBUG.println(totl_Elmnt);
-        //a = strt_pos;
       } else if ((a > 4) && (a <= totl_Elmnt)) {
-        //totl_Elmnt = decimalVal;
-        //DEBUG.print("\t TOTL ELM : "); DEBUG.println(totl_Elmnt);
-        //strt_pos;
-        //Serial.print("RX DATA : "); Serial.print(ptr);
-        //DEBUG.print(" Val of A : "); DEBUG.print(strt_pos);
-        DEBUG.print(" Val A : ");
-        DEBUG.print(a);
-        DEBUG.print("\t ADDR : ");
-        DEBUG.print(strt_pos);
-        DEBUG.print("\t DECM : ");
-        DEBUG.println(decimalVal);
-        writeI2CByte(strt_pos, decimalVal);
-        //EE_Data[c] = decimalVal;
+        if (IDU_EE_WR_FLG == 1 || ODU_EE_WR_FLG == 1) {
+          writeI2CByte(strt_pos, decimalVal);
+          delay(10);
+        }
         strt_pos++;
       }
     }
@@ -142,32 +118,27 @@ static void UART_ISR_ROUTINE(void *pvParameters)  //uart1
         int UART1_data_length = 0;
         ESP_ERROR_CHECK(uart_get_buffered_data_len(UART_NUM_1, (size_t *)&UART1_data_length));
         UART1_data_length = uart_read_bytes(UART_NUM_1, UART1_data, UART1_data_length, 100);
-        for (byte i = 0; i < UART1_data_length; i++) {
-          DEBUG.print((int)UART1_data[i]);
-          DEBUG.print(",");
-          
-        }
-        DEBUG.println("");
-
         if (UART1_data_length == ODU_RSPNS_PAC_SIZE) {
-          if (UART1_data[0] == 187) {// && UART1_data[1] == 2) {
+          if (UART1_data[0] == 187 && UART1_data[1] == 2) {
             for (byte i = 0; i < UART1_data_length; i++) {
               ODU_PAC_TWO_Temp += (int)UART1_data[i];
               ODU_PAC_TWO_Temp += ",";
             }
             ODU_PAC_TWO = ODU_PAC_TWO_Temp;
-          } else if (UART1_data[0] == 170 ) {//&& UART1_data[1] == 20) {
+          } else if (UART1_data[0] == 170 && UART1_data[1] == 20) {
             for (byte i = 0; i < UART1_data_length; i++) {
               ODU_PAC_Three_Temp += (int)UART1_data[i];
               ODU_PAC_Three_Temp += ",";
             }
             ODU_PAC_THREE = ODU_PAC_Three_Temp;
           } else {
-            for (byte i = 0; i < UART1_data_length; i++) {
-              ODU_PAC_ONE_Temp += (int)UART1_data[i];
-              ODU_PAC_ONE_Temp += ",";
+            if (UART1_data[0] == 170) {
+              for (byte i = 0; i < UART1_data_length; i++) {
+                ODU_PAC_ONE_Temp += (int)UART1_data[i];
+                ODU_PAC_ONE_Temp += ",";
+              }
+              ODU_PAC_ONE = ODU_PAC_ONE_Temp;
             }
-            ODU_PAC_ONE = ODU_PAC_ONE_Temp;
           }
         }
         ODU_PAC_ONE_Temp = "";
@@ -217,8 +188,7 @@ void test_sim800_module() {
 }
 
 void gsm_config_gprs() {
-  Serial.println(" --- CONFIG GPRS --- ");
-
+  DEBUG.println(" --- CONFIG GPRS --- ");
   MODEM.println("AT+SAPBR=3,1,Contype,GPRS");
   updateSerial();
   MODEM.println("AT+SAPBR=3,1,APN," + apn);
@@ -234,8 +204,8 @@ void gsm_config_gprs() {
 }
 
 void Send_GET_Rqst(String Data, String methodPram) {
-  DEBUG.println("Sending Data To Server: ");
-  DEBUG.println("[URL] : " + Data);
+  //DEBUG.println("Sending Data To Server: ");
+  //DEBUG.println("[URL] : " + Data);
   MODEM.println("AT+HTTPINIT\r\n");
   updateSerial();
   MODEM.println("AT+HTTPPARA=\"CID\",1\r\n");
@@ -260,6 +230,8 @@ void Send_GET_Rqst(String Data, String methodPram) {
   getData();
   MODEM.println("AT + HTTPTERM\r\n");
   updateSerial();
+  IDU_EE_DATA = "";
+  ODU_EE_DATA = "";
 }
 
 void writeI2CByte(int wr_data_addr, int wrtData) {
@@ -271,21 +243,17 @@ void writeI2CByte(int wr_data_addr, int wrtData) {
   delay(10);
 }
 
-void read_data() {
+void read_EE_data(int buff_size) {
   short tempRdData;
-  Serial.println("IDU Data Reading Start: ");
-  for (int i = 0; i < arr_counter; i++) {
-    Serial.print("ADD: ");
-    Serial.print(i);
+  IDU_EE_DATA = "";
+  for (int i = 0; i < buff_size; i++) {
     tempRdData = readI2CByte(i);
-    Serial.print("EE: ");
-    Serial.println(tempRdData, HEX);
-    chkSum += tempRdData;
     delay(10);
+    chkSum += tempRdData;
+    IDU_EE_DATA += (tempRdData);
+    IDU_EE_DATA += (",");
   }
-  Serial.println();
-  Serial.print("Check Sum: ");
-  Serial.println(chkSum, HEX);
+  IDU_EE_DATA += (chkSum);
   chkSum = 0;
 }
 
@@ -315,18 +283,9 @@ int detect_ee() {
   return nDevices;
 }
 void readCompModel() {
-  // for (unsigned int on_B = 0; on_B < 4; on_B++) {
-  //   const char x = (int16_t)compModel[on_B];
-  //   uart_write_bytes(UART_NUM_1, &x, 1);
-  // }
-  uart_write_bytes(UART_NUM_1, &compModel[0], 1);
-  delay(2);
-  uart_write_bytes(UART_NUM_1, &compModel[1], 1);
-  delay(2);
-  uart_write_bytes(UART_NUM_1, &compModel[2], 1);
-  delay(2);
-  uart_write_bytes(UART_NUM_1, &compModel[3], 1);
-  delay(2);
-  uart_write_bytes(UART_NUM_1, &compModel[4], 1);
-  delay(2);
+  for (unsigned int on_B = 0; on_B <= 4; on_B++) {
+    const char x = (int16_t)compModel[on_B];
+    uart_write_bytes(UART_NUM_1, &x, 1);
+  }
+  uart_wait_tx_done(UART_NUM_1, 300);
 }
--- RMS_SIM800L_CSD/defines.h
+++ RMS_SIM800L_CSD/defines.h
@@ -19,8 +19,8 @@ BluetoothSerial SerialBT;
 #define MODEM Serial2
 #define MODEM_BAUD_RATE 9600
 #define UART1_ODU_BAUD_RATE 2400
-#define DEBUG SerialBT 
-//#define DEBUG Serial 
+#define DEBUG SerialBT
+//#define DEBUG Serial
 #define DEBUG_BAUD_RATE 9600
 #define U1RXD 33
 #define U1TXD 32
@@ -34,9 +34,11 @@ BluetoothSerial SerialBT;
 int oduData[ODU_RSPNS_PAC_SIZE];
 int mode = 0;
 int eePortType = 0;
-
-
-// APN information for GPRS  Communication 
+int IDU_EE_RD_FLG = 0;
+int IDU_EE_WR_FLG = 0;
+int ODU_EE_RD_FLG = 0;
+int ODU_EE_WR_FLG = 0;
+// APN information for GPRS  Communication
 String apn = "internet";
 String apn_u = "";
 String apn_p = "";
@@ -50,7 +52,7 @@ String PRAM = "";
 String URI = SRV_IP + PROJECT_PATH;
 String URL = URI;
 String srvResPns = "";
-String sigLvl = "";
+String sigLvl = "0";
 String url_pram = "";
 String ODU_PAC_ONE = "";
 String ODU_PAC_TWO = "";
@@ -59,7 +61,7 @@ String IDU_EE_DATA = "";
 String ODU_EE_DATA = "";
 String MOB_NO = "01608984560";
 
-short chkSum = 0, chkData = 0, arr_counter = 256;
+short chkData = 0, arr_counter = 256;
+int chkSum = 0;
 
-
-byte compModel[5]  = {0xAA, 0x05, 0x00, 0xFB, 0x55};
+byte compModel[5] = { 0xAA, 0x05, 0x00, 0xFB, 0x55 };
